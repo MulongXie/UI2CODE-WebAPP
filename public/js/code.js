ee1ad420dd91cdb7ce418248e713cf324d013323
MulongXie <dsh15325@163.com>
2020-09-21 10:24:09 +1000

    highlight the quotation

--- public/js/code.js
+++ public/js/code.js
@@ -16,42 +16,67 @@ $(document).ready(function () {
 
         let tag = ''
         let search_tag = false
+        let quotation = ''
+        let search_quotation = false
         let content = ''
 
         for(let i = 0; i < line.length; i++){
             let c = line[i]
 
             if(search_tag){
+                // end the tag by space
                 if(c === ' '){
                     html += '<span class="html-tag">' + tag + c + '</span>'
                     tag = ''
                     search_tag = false
                 }
+                // end the tag by ">"
                 else if(c === '>'){
                     html += '<span class="html-tag">' + tag + '</span>' +
                         '<span class="html-bracket">' + c + '</span>'
                     tag = ''
                     search_tag = false
                 }
+                // record the tag
                 else {
                     tag += c
                 }
+            }
+            else if(search_quotation){
+                // end the quotation
+                if (c === '"'){
+                    html += '<span class="html-quotation">' + quotation + c + '</span>'
+                    quotation = ''
+                    search_quotation = false
+                }
+                // record the quotation
+                else{
+                    quotation += c
+                }
+            }
+            else if (c === '"'){
+                if(content !== ''){
+                    html += '<span>' + content + '</span>'
+                    content = ''
+                }
+                quotation = c
+                search_quotation = true
             }
             else if(c === '<'){
                 if(content !== ''){
                     html += '<span>' + content + '</span>'
                     content = ''
                 }
-                html += '<span class="html-bracket">' + line[i] + '</span>'
+                html += '<span class="html-bracket">' + c + '</span>'
                 search_tag = true
             }
             else if(c === '>'){
                 html += '<span>' + content + '</span>'
-                html += '<span class="html-bracket">' + line[i] + '</span>'
+                html += '<span class="html-bracket">' + c + '</span>'
                 content = ''
             }
             else {
-                content += line[i]
+                content += c
             }
         }
         return html
